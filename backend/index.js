f023bc573a2ecee147a2e64e38c7621ffcdafe23
sreedhar111yu <sreedharv36@gmail.com>
2024-02-14 09:46:14 +0530

    setup backend and database

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('MongoDb is connected');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const app=express();
+
+app.listen(4000,()=>{
+    console.log('Server running on port 4000!')
+})
