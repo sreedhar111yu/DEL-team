aab05cc3d12a6a16f482579c6fbf598f08338dc3
sreedhar111yu <sreedharv36@gmail.com>
2024-02-28 10:01:21 +0530

    auth compeleted

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,31 +1,29 @@
-import React from 'react'
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import Navbar from './Components/Navbar';
-import Home from './pages/Home';
-import Signin from './pages/Signin';
-import Signup from './pages/Signup';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home'
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Profile from './pages/Profile';
+import Header from './components/Header';
+import PrivateRoute from './components/PrivateRouter';
 
 function App() {
   return (
-    <>
-    <BrowserRouter>
-    <Navbar/>
-    <Routes>
-    <Route path='/' element={<Home/>}/>
-    <Route path='/signin' element={<Signin/>}/>
-     <Route path='/signup' element={<Signup/>}/>
-
-
-
-    </Routes>
-    
-    
+   <>
+   <BrowserRouter>
+      {/* header */}
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route element={<PrivateRoute />}>
+          <Route path='/profile' element={<Profile />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
-
-
-    
-    </>
+   </>
   )
 }
 
